4b3f89c797a8720ef6b089b1ee3bed60c0dd41f7
Hyejin Kim <cshjkim@gmail.com>
2020-01-07 23:16:51 -0800

    add function for first prompt

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -13,14 +13,47 @@ var connection = mysql.createConnection({
 
 connection.connect(function(err) {
     if (err) throw err;
-    runSearch();
+    loadProduct();
 })
 
 // display all of the items available for sale 
 
+function loadProduct() {
+    connection.query("SELECT * FROOM products", function(err,res) {
+        if (err) throw err;
+        console.table(res);
+        askCustomer(res);
+    });
+}
+
 // inquire prompt 1. ask them the id of the product 2. how many units of the product
 
+function askCustomer(inventory) {
+    inquirer
+    .prompt([{
+        name: "choice",
+        type: "input",
+        message: "What is the ID of the item you would you like to purchase? [Quit with Q}",
+        validate: function(val) {
+            return !isNaN(Val) || val.toLowerCase() === "q";
+        }
+    }])
+    .then(function(val) {
+       checkIfWantExit(val.choice);
+       var choiceID = parseInt(val.choice);
+       var product = checkInventory(choiceID, inventory);
+
+       if (product) {
+           askCustomerTwo(product);
+       }
+       else {
+        console.log("That item is not in the inventory");
+        loadProduct();
+       }
+    });
+}
 // prompt to check if your store has enought of the product to meet customer's request
+
 
 // results - phrase "insufficient quantity!" or fulfill the customer's order (updating the SQL database & customer's total cost of their purchase)
 
