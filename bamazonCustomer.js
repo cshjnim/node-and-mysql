185c5375f9ce0c252e0a615ece1f00e5578c35bc
Hyejin Kim <cshjkim@gmail.com>
2020-01-07 18:27:54 -0800

    pseudocode for homework instructions

---
+++ bamazonCustomer.js
@@ -0,0 +1,10 @@
+// node JS - mysql & inquire
+
+// display all of the items available for sale 
+
+// inquire prompt 1. ask them the id of the product 2. how many units of the product
+
+// prompt to check if your store has enought of the product to meet customer's request
+
+// results - phrase "insufficient quantity!" or fulfill the customer's order (updating the SQL database & customer's total cost of their purchase)
+
---
+++ bamazonManager.js
@@ -0,0 +1,11 @@
+// node JS - sql & function
+
+// list a set of menu options (view procuts for sale, view low inventory, add to inventory, add new product)
+
+// view products for sale function
+
+// view low inventory function
+
+// add to inventory function
+
+// add new product function
