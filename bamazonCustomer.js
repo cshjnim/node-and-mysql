537212b5b358a74bf9d80a0b0093724d2263f0d0
Hyejin Kim <cshjkim@gmail.com>
2020-01-07 23:24:07 -0800

    add function for second prompts and add quit function

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -53,7 +53,37 @@ function askCustomer(inventory) {
     });
 }
 // prompt to check if your store has enought of the product to meet customer's request
-
-
 // results - phrase "insufficient quantity!" or fulfill the customer's order (updating the SQL database & customer's total cost of their purchase)
 
+function askCustomerTwo(product) {
+    inquirer
+    .prompt([
+        {
+            type: "input",
+            name: "quantity",
+            message: "How many would you want to purchase? [Quit with Q}",
+            validate: function(val) {
+                return val > 0 || val.toLowerCase() === "q";
+            }
+        }
+    ])
+    .then(function(val) {
+        checkIfWantExit(val.quantity);
+        var quantity = parseInt(val.quantity);
+
+        if (quantity > product.stock_quantity) {
+            console.log("Insufficient Quantity!");
+            loadProduct();
+        }
+        else {
+            makePurchase(product, quantity);
+        }
+    });
+}
+
+function checkIfWantExit(choice) {
+    if(choice.toLowerCase() === "q") {
+        console.log("Bye now");
+        process.exit(0);
+    }
+}
