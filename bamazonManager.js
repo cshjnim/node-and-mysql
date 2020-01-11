e1351bd6433be2d4707622a426f230786d10e97f
Hyejin Kim <cshjkim@gmail.com>
2020-01-10 21:08:49 -0800

    start working on manager js

--- bamazonManager.js
+++ bamazonManager.js
@@ -1,4 +1,23 @@
 // node JS - sql & function
+var mysql = require ("mysql");
+var inquirer = require ("inquirer");
+require("console.table");
+
+//MySQL connection
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "986680Hj!",
+    database: "bamazon"
+});
+
+connection.connect(function(err){
+    if(err) {
+        console.error("error connecting: " + err.stack);
+    }
+    managerMenu();
+})
 
 // list a set of menu options (view procuts for sale, view low inventory, add to inventory, add new product)
 
