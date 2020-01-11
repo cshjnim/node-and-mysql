c263398ef127c26b185fb0b5aa2e5a4f70773951
Hyejin Kim <cshjkim@gmail.com>
2020-01-10 21:20:06 -0800

    add functions for cases

--- bamazonManager.js
+++ bamazonManager.js
@@ -21,9 +21,48 @@ connection.connect(function(err){
 
 // list a set of menu options (view procuts for sale, view low inventory, add to inventory, add new product)
 
+function managerMenu() {
+    connection.query("SELECT * FROM products", function(err, res) {
+        if (err) throw err;
+        managerOptions(res);
+    });
+}
+
+function managerOptions(products) {
+    inquirer
+    .prompt({
+        type: "list",
+        name: "choice",
+        choiceS: ["View Products for Sale", "View Low Inventory", "Add New Product", "Quit"]
+        message: "What do you want to check?"
+    })
+    .then(function(val){
+        switch(val.choice){
+            case "View Products for Sale":
+                console.table(products);
+                managerMenu();
+                break;
+            case "View Low Inventory":
+                loadLowInventory();
+                break;
+            case "Add to Inventory":
+                addInventory(products);
+                break;
+            case "Add New Product":
+                addNewProduct(products);
+                break;
+            default:
+                console.log("Bye-now!");
+                process.exit(0);
+                break;
+        }
+    });
+}
+
 // view products for sale function
 
 // view low inventory function
+
 
 // add to inventory function
 
